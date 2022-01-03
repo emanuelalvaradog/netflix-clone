3c0372cba41a6e039b06217652721042bff895ae
Emanuel Alvarado <emanuelalvaradog@gmail.com>
2022-01-03 16:50:40 -0600

    Responsive CSS & JS

--- netflix-front/js/loadCarousel.js
+++ netflix-front/js/loadCarousel.js
@@ -18,164 +18,298 @@ const API = "https://secure-escarpment-59792.herokuapp.com";
 const genres = await fetch(`${API}/genres`);
 const moviesList = genres.genres;
 
-moviesList.forEach(async (genre) => {
-  let observer = new IntersectionObserver(
-    (entries) => {
-      entries
-        .filter((entry) => {
-          return entry.isIntersecting;
-        })
-        .forEach((entry) => {
-          const elementContainer = entry.target;
-          const imageContainer = elementContainer.firstChild;
-          const image = imageContainer.firstChild;
-          const url = image.dataset.src;
-          image.src = url;
-          delete image.dataset.src;
-          observer.unobserve(elementContainer);
-        });
-    },
-    { rootMargin: "30px" }
-  );
+const mobileQuery = window.matchMedia("(max-width: 425px)");
 
-  // Api Data
-  const moviesByGenre = await fetch(`${API}/movies/${genre.id}`);
-
-  const parent = document.querySelector(".carouselContainer");
-  const carouselContainer = document.createElement("div");
-  carouselContainer.classList.add("carouselContainer-carousel");
-  const carouselElements = document.createElement("div");
-  carouselElements.classList.add("carousel-movies");
-
-  // Carousel title by genre
-  const carouselTitle = document.createElement("div");
-  carouselTitle.classList.add("carousel-title");
-  const carouselTitleTxt = document.createElement("p");
-  carouselTitleTxt.innerText = genre.name;
-  carouselTitle.appendChild(carouselTitleTxt);
-
-  for (let i = 0; i < moviesByGenre.results.length; i++) {
-    const element = document.createElement("div");
-    element.classList.add("movie");
-
-    // Poster from movies array
-    const elementPosterContainer = document.createElement("div");
-    elementPosterContainer.classList.add("movie-poster");
-    const elementPoster = document.createElement("img");
-    elementPoster.dataset.src = `https://image.tmdb.org/t/p/original${moviesByGenre.results[i].poster_path}`;
-    elementPosterContainer.appendChild(elementPoster);
-
-    // Controls taken from static image
-    const elementControls = document.createElement("div");
-    elementControls.classList.add("movie-controls");
-    const playControl = document.createElement("img");
-    playControl.src = "./netflix-front/img/play.png";
-    const addControl = document.createElement("img");
-    addControl.src = "./netflix-front/img/add.png";
-    const likeControl = document.createElement("img");
-    likeControl.src = "./netflix-front/img/like.png";
-    const dislikeControl = document.createElement("img");
-    dislikeControl.src = "./netflix-front/img/dislike.png";
-    const moreControl = document.createElement("img");
-    moreControl.src = "./netflix-front/img/more.png";
-
-    elementControls.append(
-      playControl,
-      addControl,
-      likeControl,
-      dislikeControl,
-      moreControl
+if (!mobileQuery.matches) {
+  moviesList.forEach(async (genre) => {
+    let observer = new IntersectionObserver(
+      (entries) => {
+        entries
+          .filter((entry) => {
+            return entry.isIntersecting;
+          })
+          .forEach((entry) => {
+            const elementContainer = entry.target;
+            const imageContainer = elementContainer.firstChild;
+            const image = imageContainer.firstChild;
+            const url = image.dataset.src;
+            image.src = url;
+            delete image.dataset.src;
+            observer.unobserve(elementContainer);
+          });
+      },
+      { rootMargin: "30px" }
     );
 
-    const elementInfo = document.createElement("div");
-    elementInfo.classList.add("movie-info");
+    // Api Data
+    const moviesByGenre = await fetch(`${API}/movies/${genre.id}`);
 
-    // Movie Title
-    const movieTitle = document.createElement("div");
-    movieTitle.classList.add("movie-title");
-    const movieTitleTxt = document.createElement("p");
-    movieTitleTxt.innerText = moviesByGenre.results[i].title;
-    movieTitle.appendChild(movieTitleTxt);
+    const parent = document.querySelector(".carouselContainer");
+    const carouselContainer = document.createElement("div");
+    carouselContainer.classList.add("carouselContainer-carousel");
+    const carouselElements = document.createElement("div");
+    carouselElements.classList.add("carousel-movies");
 
-    // Random likelihood number
-    const likelihoodInfo = document.createElement("div");
-    likelihoodInfo.classList.add("likelihood");
-    const likelihoodInfoTxt = document.createElement("p");
-    const likenum = Math.floor(Math.random() * (84 - 100 + 1) + 84);
-    likelihoodInfoTxt.innerText = likenum;
-    likelihoodInfo.appendChild(likelihoodInfoTxt);
+    // Carousel title by genre
+    const carouselTitle = document.createElement("div");
+    carouselTitle.classList.add("carousel-title");
+    const carouselTitleTxt = document.createElement("p");
+    carouselTitleTxt.innerText = genre.name;
+    carouselTitle.appendChild(carouselTitleTxt);
 
-    // Random movie length
-    const lengthInfo = document.createElement("div");
-    lengthInfo.classList.add("length");
-    const lengthInfoTxt = document.createElement("p");
-    const lengthnum = Math.floor(Math.random() * (68 - 137 + 1) + 68);
-    lengthInfoTxt.innerText = lengthnum;
-    lengthInfo.appendChild(lengthInfoTxt);
+    for (let i = 0; i < moviesByGenre.results.length; i++) {
+      const element = document.createElement("div");
+      element.classList.add("movie");
 
-    // Set genre names from object
-    const genresInfo = document.createElement("div");
-    genresInfo.classList.add("genres");
-    for (let j = 0; j < moviesByGenre.results[i].genre_ids.length; j++) {
-      const genreInfoTxt = document.createElement("p");
-      moviesList.map((genre) => {
-        if (genre.id == moviesByGenre.results[i].genre_ids[j]) {
-          genreInfoTxt.innerText = genre.name;
-          genresInfo.appendChild(genreInfoTxt);
-        }
-      });
+      // Poster from movies array
+      const elementPosterContainer = document.createElement("div");
+      elementPosterContainer.classList.add("movie-poster");
+      const elementPoster = document.createElement("img");
+      elementPoster.dataset.src = `https://image.tmdb.org/t/p/original${moviesByGenre.results[i].poster_path}`;
+      elementPosterContainer.appendChild(elementPoster);
+
+      const elementInfo = document.createElement("div");
+      elementInfo.classList.add("movie-info");
+
+      // Movie Title
+      const movieTitle = document.createElement("div");
+      movieTitle.classList.add("movie-title");
+      const movieTitleTxt = document.createElement("p");
+      movieTitleTxt.innerText = moviesByGenre.results[i].title;
+      movieTitle.appendChild(movieTitleTxt);
+
+      const movieNumbers = document.createElement("div");
+      movieNumbers.classList.add("movie-numbers");
+
+      // Random likelihood number
+      const likelihoodInfo = document.createElement("div");
+      likelihoodInfo.classList.add("likelihood");
+      const likelihoodInfoTxt = document.createElement("p");
+      const likenum = Math.floor(Math.random() * (100 - 84 + 1) + 84);
+      likelihoodInfoTxt.innerText = `${likenum}%`;
+      likelihoodInfo.appendChild(likelihoodInfoTxt);
+
+      // Random movie length
+      const lengthInfo = document.createElement("div");
+      lengthInfo.classList.add("length");
+      const lengthInfoTxt = document.createElement("p");
+      const lengthnum = Math.floor(Math.random() * (137 - 68 + 1) + 68);
+      lengthInfoTxt.innerText = `${lengthnum} min`;
+      lengthInfo.appendChild(lengthInfoTxt);
+
+      // Set genre names from object
+      const genresInfo = document.createElement("div");
+      genresInfo.classList.add("genres");
+      for (let j = 0; j < moviesByGenre.results[i].genre_ids.length; j++) {
+        const genreInfoTxt = document.createElement("p");
+        moviesList.map((genre) => {
+          if (genre.id == moviesByGenre.results[i].genre_ids[j]) {
+            genreInfoTxt.innerText = genre.name;
+            genresInfo.appendChild(genreInfoTxt);
+          }
+        });
+      }
+
+      movieNumbers.append(likelihoodInfo, lengthInfo);
+
+      const overviewInfo = document.createElement("div");
+      overviewInfo.classList.add("overview");
+      const overviewInfoTxt = document.createElement("p");
+      overviewInfoTxt.innerText = moviesByGenre.results[i].overview;
+      overviewInfo.appendChild(overviewInfoTxt);
+
+      elementInfo.append(movieTitle, movieNumbers, genresInfo, overviewInfo);
+
+      element.append(elementPosterContainer, elementInfo);
+      observer.observe(element);
+      carouselElements.append(element);
+      carouselContainer.append(carouselTitle, carouselElements);
     }
 
-    const overviewInfo = document.createElement("div");
-    overviewInfo.classList.add("overview");
-    const overviewInfoTxt = document.createElement("p");
-    overviewInfoTxt.innerText = moviesByGenre.results[i].overview;
-    overviewInfo.appendChild(overviewInfoTxt);
+    const nodes = [...carouselElements.children].slice(0);
+    nodes.forEach((el) => {
+      el.addEventListener("mouseenter", (n) => {
+        const node = n.currentTarget;
+        node.classList.add("card");
+      });
 
-    elementInfo.append(
-      movieTitle,
-      likelihoodInfo,
-      lengthInfo,
-      genresInfo,
-      overviewInfo
+      el.addEventListener("mouseleave", (n) => {
+        const node = n.currentTarget;
+        node.classList.remove("card");
+      });
+    });
+
+    // Carousel controls
+    const carouselControls = document.createElement("div");
+    carouselControls.className = "carouselControls";
+    const leftContainer = document.createElement("span");
+    const leftControl = document.createElement("i");
+    leftControl.className = "fas fa-chevron-left prev";
+
+    leftContainer.addEventListener("click", () => {
+      const offset = carouselElements.offsetWidth;
+      carouselElements.scrollLeft -= offset;
+    });
+
+    leftContainer.appendChild(leftControl);
+    const rightContainer = document.createElement("span");
+    const rightControl = document.createElement("i");
+    rightControl.className = "fas fa-chevron-right next";
+    rightContainer.appendChild(rightControl);
+
+    rightContainer.addEventListener("click", () => {
+      const offset = carouselElements.offsetWidth;
+      carouselElements.scrollLeft += offset;
+    });
+    carouselControls.append(leftContainer, rightContainer);
+
+    carouselElements.insertAdjacentElement("afterbegin", carouselControls);
+    parent.appendChild(carouselContainer);
+  });
+} else {
+  moviesList.forEach(async (genre) => {
+    let observer = new IntersectionObserver(
+      (entries) => {
+        entries
+          .filter((entry) => {
+            return entry.isIntersecting;
+          })
+          .forEach((entry) => {
+            const elementContainer = entry.target;
+            const imageContainer = elementContainer.firstChild;
+            const image = imageContainer.firstChild;
+            const url = image.dataset.src;
+            image.src = url;
+            delete image.dataset.src;
+            observer.unobserve(elementContainer);
+          });
+      },
+      { rootMargin: "30px" }
     );
 
-    element.append(elementPosterContainer, elementControls, elementInfo);
-    observer.observe(element);
-    carouselElements.append(element);
-    carouselContainer.append(carouselTitle, carouselElements);
-  }
+    // Api Data
+    const moviesByGenre = await fetch(`${API}/movies/${genre.id}`);
 
-  // element.forEach((el)=>{
-  //   el.addEventListener("mouseenter", (n)=>{
-  //     const node = n.currentTarget;
-  //   })
-  // })
+    const parent = document.querySelector(".carouselContainer");
+    const carouselContainer = document.createElement("div");
+    carouselContainer.classList.add("carouselContainer-carousel");
+    const carouselElements = document.createElement("div");
+    carouselElements.classList.add("carousel-movies");
 
-  // Carousel controls
-  const carouselControls = document.createElement("div");
-  carouselControls.className = "carouselControls";
-  const leftContainer = document.createElement("span");
-  const leftControl = document.createElement("i");
-  leftControl.className = "fas fa-chevron-left prev";
+    // Carousel title by genre
+    const carouselTitle = document.createElement("div");
+    carouselTitle.classList.add("carousel-title");
+    const carouselTitleTxt = document.createElement("p");
+    carouselTitleTxt.innerText = genre.name;
+    carouselTitle.appendChild(carouselTitleTxt);
 
-  leftContainer.addEventListener("click", () => {
-    const offset = carouselElements.offsetWidth;
-    carouselElements.scrollLeft -= offset;
+    for (let i = 0; i < moviesByGenre.results.length; i++) {
+      const element = document.createElement("div");
+      element.classList.add("movie");
+
+      // Poster from movies array
+      const elementPosterContainer = document.createElement("div");
+      elementPosterContainer.classList.add("movie-poster");
+      const elementPoster = document.createElement("img");
+      elementPoster.dataset.src = `https://image.tmdb.org/t/p/original${moviesByGenre.results[i].poster_path}`;
+      elementPosterContainer.appendChild(elementPoster);
+
+      const elementInfo = document.createElement("div");
+      elementInfo.classList.add("movie-info");
+
+      // Movie Title
+      const movieTitle = document.createElement("div");
+      movieTitle.classList.add("movie-title");
+      const movieTitleTxt = document.createElement("p");
+      movieTitleTxt.innerText = moviesByGenre.results[i].title;
+      movieTitle.appendChild(movieTitleTxt);
+
+      const movieNumbers = document.createElement("div");
+      movieNumbers.classList.add("movie-numbers");
+
+      // Random likelihood number
+      const likelihoodInfo = document.createElement("div");
+      likelihoodInfo.classList.add("likelihood");
+      const likelihoodInfoTxt = document.createElement("p");
+      const likenum = Math.floor(Math.random() * (100 - 84 + 1) + 84);
+      likelihoodInfoTxt.innerText = `${likenum}%`;
+      likelihoodInfo.appendChild(likelihoodInfoTxt);
+
+      // Random movie length
+      const lengthInfo = document.createElement("div");
+      lengthInfo.classList.add("length");
+      const lengthInfoTxt = document.createElement("p");
+      const lengthnum = Math.floor(Math.random() * (137 - 68 + 1) + 68);
+      lengthInfoTxt.innerText = `${lengthnum} min`;
+      lengthInfo.appendChild(lengthInfoTxt);
+
+      // Set genre names from object
+      const genresInfo = document.createElement("div");
+      genresInfo.classList.add("genres");
+      for (let j = 0; j < moviesByGenre.results[i].genre_ids.length; j++) {
+        const genreInfoTxt = document.createElement("p");
+        moviesList.map((genre) => {
+          if (genre.id == moviesByGenre.results[i].genre_ids[j]) {
+            genreInfoTxt.innerText = genre.name;
+            genresInfo.appendChild(genreInfoTxt);
+          }
+        });
+      }
+
+      movieNumbers.append(likelihoodInfo, lengthInfo);
+
+      const overviewInfo = document.createElement("div");
+      overviewInfo.classList.add("overview");
+      const overviewInfoTxt = document.createElement("p");
+      overviewInfoTxt.innerText = moviesByGenre.results[i].overview;
+      overviewInfo.appendChild(overviewInfoTxt);
+
+      elementInfo.append(movieTitle, movieNumbers, genresInfo, overviewInfo);
+
+      element.append(elementPosterContainer, elementInfo);
+      observer.observe(element);
+      carouselElements.append(element);
+      carouselContainer.append(carouselTitle, carouselElements);
+    }
+
+    // const nodes = [...carouselElements.children].slice(0);
+    // nodes.forEach((el) => {
+    //   el.addEventListener("mouseenter", (n) => {
+    //     const node = n.currentTarget;
+    //     node.classList.add("card");
+    //   });
+
+    //   el.addEventListener("mouseleave", (n) => {
+    //     const node = n.currentTarget;
+    //     node.classList.remove("card");
+    //   });
+    // });
+
+    // // Carousel controls
+    // const carouselControls = document.createElement("div");
+    // carouselControls.className = "carouselControls";
+    // const leftContainer = document.createElement("span");
+    // const leftControl = document.createElement("i");
+    // leftControl.className = "fas fa-chevron-left prev";
+
+    // leftContainer.addEventListener("click", () => {
+    //   const offset = carouselElements.offsetWidth;
+    //   carouselElements.scrollLeft -= offset;
+    // });
+
+    // leftContainer.appendChild(leftControl);
+    // const rightContainer = document.createElement("span");
+    // const rightControl = document.createElement("i");
+    // rightControl.className = "fas fa-chevron-right next";
+    // rightContainer.appendChild(rightControl);
+
+    // rightContainer.addEventListener("click", () => {
+    //   const offset = carouselElements.offsetWidth;
+    //   carouselElements.scrollLeft += offset;
+    // });
+    // carouselControls.append(leftContainer, rightContainer);
+
+    // carouselElements.insertAdjacentElement("afterbegin", carouselControls);
+    parent.appendChild(carouselContainer);
   });
-
-  leftContainer.appendChild(leftControl);
-  const rightContainer = document.createElement("span");
-  const rightControl = document.createElement("i");
-  rightControl.className = "fas fa-chevron-right next";
-  rightContainer.appendChild(rightControl);
-
-  rightContainer.addEventListener("click", () => {
-    const offset = carouselElements.offsetWidth;
-    carouselElements.scrollLeft += offset;
-  });
-  carouselControls.append(leftContainer, rightContainer);
-
-  carouselElements.insertAdjacentElement("afterbegin", carouselControls);
-  parent.appendChild(carouselContainer);
-});
+}
--- netflix-front/js/mainMovie.js
+++ netflix-front/js/mainMovie.js
@@ -1,5 +1,119 @@
-class PlayerControl {
-  constructor(
+const mobileQuery = window.matchMedia("(max-width: 425px)");
+const app = document.querySelector(".app");
+
+if (!mobileQuery.matches) {
+  app.classList.add("destkop");
+  class PlayerControl {
+    constructor(
+      video,
+      img,
+      videoContainer,
+      muted,
+      unmuted,
+      repeat,
+      controlsContainer,
+      dataContainer,
+      videoObserver
+    ) {
+      this.video = video;
+      this.img = img;
+      this.videoContainer = videoContainer;
+      this.muted = muted;
+      this.unmuted = unmuted;
+      this.repeat = repeat;
+      this.controlsContainer = controlsContainer;
+      this.dataContainer = dataContainer;
+      this.videoObserver = videoObserver;
+    }
+
+    loadStatic() {
+      videoContainer.removeChild(videoContainer.children[0]);
+      videoContainer.appendChild(img);
+      controlsContainer.removeChild(controlsContainer.children[0]);
+      controlsContainer.appendChild(repeat);
+      dataContainer.children[1].style.opacity = "1";
+      dataContainer.style.top = "18vw";
+      dataContainer.children[0].style.width = "35rem";
+      dataContainer.children[1].style.visibility = "visible";
+    }
+
+    loadVideo() {
+      videoContainer.removeChild(videoContainer.children[0]);
+      videoContainer.appendChild(video);
+      controlsContainer.removeChild(controlsContainer.children[0]);
+      controlsContainer.appendChild(muted);
+      video.muted = true;
+      video.play();
+      setTimeout(() => {
+        dataContainer.style.top = "55vh";
+        dataContainer.children[0].style.width = "32rem";
+        dataContainer.children[1].style.top = "22vh";
+        dataContainer.children[1].style.opacity = "0";
+        dataContainer.children[1].style.visibility = "hidden";
+      }, 3500);
+    }
+
+    listenVideo(isObserving) {
+      if (!isObserving) {
+        videoObserver.unobserve(videoContainer);
+        videoObserver.disconnect();
+        this.loadStatic();
+      } else {
+        this.loadVideo();
+        videoObserver.observe(videoContainer);
+      }
+    }
+
+    played() {
+      return !video.played.length == 0;
+    }
+
+    muteVideo() {
+      if (video.muted) {
+        controlsContainer.removeChild(muted);
+        controlsContainer.appendChild(unmuted);
+        video.muted = false;
+      } else {
+        controlsContainer.removeChild(unmuted);
+        controlsContainer.append(muted);
+        video.muted = true;
+      }
+    }
+  }
+
+  const videoObserver = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        console.log("Intersecting");
+        player.loadVideo();
+      } else {
+        console.log("Not intersecting");
+        player.loadStatic();
+      }
+    },
+    { threshold: 0.6 }
+  );
+
+  const videoContainer = document.querySelector(".mainMovie-video");
+  const controlsContainer = document.querySelector(".mainMovie-controls");
+  const dataContainer = document.querySelector(".mainMovie-data");
+
+  const img = document.createElement("img");
+  img.src = "./netflix-front/img/klausStatic.jpg";
+
+  const video = document.createElement("video");
+  video.src = "./netflix-front/img/KlausTrailer.mp4";
+
+  const muted = document.createElement("img");
+  muted.src = "./netflix-front/img/muted.png";
+
+  const unmuted = document.createElement("img");
+  unmuted.src = "./netflix-front/img/unmuted.png";
+
+  const repeat = document.createElement("img");
+  repeat.src = "./netflix-front/img/repeat.png";
+
+  const player = new PlayerControl(
     video,
     img,
     videoContainer,
@@ -9,134 +123,33 @@ class PlayerControl {
     controlsContainer,
     dataContainer,
     videoObserver
-  ) {
-    this.video = video;
-    this.img = img;
-    this.videoContainer = videoContainer;
-    this.muted = muted;
-    this.unmuted = unmuted;
-    this.repeat = repeat;
-    this.controlsContainer = controlsContainer;
-    this.dataContainer = dataContainer;
-    this.videoObserver = videoObserver;
-  }
+  );
 
-  loadStatic() {
-    videoContainer.removeChild(videoContainer.children[0]);
-    videoContainer.appendChild(img);
-    controlsContainer.removeChild(controlsContainer.children[0]);
-    controlsContainer.appendChild(repeat);
-    dataContainer.children[1].style.opacity = "1";
-    dataContainer.style.top = "18vw";
-    dataContainer.children[0].style.width = "35rem";
-    dataContainer.children[1].style.visibility = "visible";
-  }
-
-  loadVideo() {
-    videoContainer.removeChild(videoContainer.children[0]);
-    videoContainer.appendChild(video);
-    controlsContainer.removeChild(controlsContainer.children[0]);
-    controlsContainer.appendChild(muted);
-    video.muted = true;
-    video.play();
-    setTimeout(() => {
-      dataContainer.style.top = "55vh";
-      dataContainer.children[0].style.width = "32rem";
-      dataContainer.children[1].style.top = "22vh";
-      dataContainer.children[1].style.opacity = "0";
-      dataContainer.children[1].style.visibility = "hidden";
-    }, 3500);
-  }
-
-  listenVideo(isObserving) {
-    if (!isObserving) {
-      videoObserver.unobserve(videoContainer);
-      videoObserver.disconnect();
-      this.loadStatic();
-    } else {
-      this.loadVideo();
-      videoObserver.observe(videoContainer);
-    }
-  }
-
-  played() {
-    return !video.played.length == 0;
-  }
-
-  muteVideo() {
-    if (video.muted) {
-      controlsContainer.removeChild(muted);
-      controlsContainer.appendChild(unmuted);
-      video.muted = false;
-    } else {
-      controlsContainer.removeChild(unmuted);
-      controlsContainer.append(muted);
-      video.muted = true;
-    }
-  }
-}
-
-const videoObserver = new IntersectionObserver(
-  (entries) => {
-    if (entries[0].isIntersecting) {
-      console.log("Intersecting");
-      player.loadVideo();
-    } else {
-      console.log("Not intersecting");
-      player.loadStatic();
-    }
-  },
-  { threshold: 0.6 }
-);
-
-const videoContainer = document.querySelector(".mainMovie-video");
-const controlsContainer = document.querySelector(".mainMovie-controls");
-const dataContainer = document.querySelector(".mainMovie-data");
-
-const img = document.createElement("img");
-img.src = "./netflix-front/img/klausStatic.jpg";
-
-const video = document.createElement("video");
-video.src = "./netflix-front/img/KlausTrailer.mp4";
-
-const muted = document.createElement("img");
-muted.src = "./netflix-front/img/muted.png";
-
-const unmuted = document.createElement("img");
-unmuted.src = "./netflix-front/img/unmuted.png";
-
-const repeat = document.createElement("img");
-repeat.src = "./netflix-front/img/repeat.png";
-
-const player = new PlayerControl(
-  video,
-  img,
-  videoContainer,
-  muted,
-  unmuted,
-  repeat,
-  controlsContainer,
-  dataContainer,
-  videoObserver
-);
-
-const button = document.querySelector(".mainMovie-controls");
-button.addEventListener("click", () => {
-  if (button.firstElementChild.src.includes("repeat")) {
-    player.listenVideo(true);
-  } else {
-    player.muteVideo();
-  }
-});
-
-window.onload = () => {
-  setTimeout(() => {
-    if (!player.played()) {
+  const button = document.querySelector(".mainMovie-controls");
+  button.addEventListener("click", () => {
+    if (button.firstElementChild.src.includes("repeat")) {
       player.listenVideo(true);
+    } else {
+      player.muteVideo();
     }
-  }, 5000);
-};
+  });
 
-video.addEventListener("ended", () => {
-  player.listenVideo(false);
-});
+  window.onload = () => {
+    setTimeout(() => {
+      if (!player.played()) {
+        player.listenVideo(true);
+      }
+    }, 5000);
+  };
+
+  video.addEventListener("ended", () => {
+    player.listenVideo(false);
+  });
+} else {
+  app.classList.add("mobile");
+  const header = document.querySelector(".header-container");
+  app.removeChild(app.firstElementChild);
+  header.removeChild(header.children[1]);
+  header.children[0].removeChild(header.children[0].children[1]);
+  // const controls = document.querySelectorAll(".");
+}
