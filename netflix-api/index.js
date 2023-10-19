ec4401bb336416157bafbb9a458d45d070065801
emanuelalvaradog <emanuel.alvarado.g@gmail.com>
2023-10-19 10:03:15 -0600

    fix: change docs

--- netflix-api/index.js
+++ netflix-api/index.js
@@ -44,5 +44,25 @@ app.get("/movies/:genreNumber", async (req, res, next) => {
   }
 });
 
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
 // Export the Express API
-module.exports = app;
+module.exports = allowCors(app);
