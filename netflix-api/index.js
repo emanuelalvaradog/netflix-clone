58a5b5a61cd1c05e3aad31997cb82d35375eb898
emanuelalvaradog <emanuel.alvarado.g@gmail.com>
2023-10-19 09:54:20 -0600

    fix: install cors

--- netflix-api/index.js
+++ netflix-api/index.js
@@ -1,9 +1,11 @@
 // index.js
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 const PORT = 4000;
 const URL = "https://api.themoviedb.org/3";
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
