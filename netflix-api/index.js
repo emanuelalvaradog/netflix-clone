53a93c3003d2fc1b6447a8187397595c5ef41ccf
emanuelalvaradog <emanuel.alvarado.g@gmail.com>
2024-11-27 14:15:51 -0600

    f: a

--- netflix-api/index.js
+++ netflix-api/index.js
@@ -1,18 +1,19 @@
-// index.js
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
+
 const app = express();
 const PORT = 4000;
 const URL = "https://api.themoviedb.org/3";
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Netflix API 🥳");
 });
 
-app.get("/genres", async (req, res, next) => {
+app.get("/genres", async (req, res) => {
   try {
     const data = await fetch(
       `${URL}/genre/movie/list?api_key=${process.env.API_KEY}&language=en-US`
@@ -22,47 +23,29 @@ app.get("/genres", async (req, res, next) => {
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: err.message,
+      message: error.message,
     });
   }
 });
 
-// Get movies by genre
-app.get("/movies/:genreNumber", async (req, res, next) => {
+app.get("/movies/:genreNumber", async (req, res) => {
   try {
     const { genreNumber } = req.params;
     const data = await fetch(
-      `${URL}/discover/movie/?api_key=${process.env.API_KEY}&language=en-US&with_genres=${genreNumber}&sort_by=popularity.desc`
+      `${URL}/discover/movie?api_key=${process.env.API_KEY}&language=en-US&with_genres=${genreNumber}&sort_by=popularity.desc`
     );
     const dataJSON = await data.json();
     res.send(dataJSON);
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: err.message,
+      message: error.message,
     });
   }
 });
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT}`);
+});
 
-// Export the Express API
-module.exports = allowCors(app);
+module.exports = app;
